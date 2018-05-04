41c14c896d018d7c275573e12a62324b9f2e8bfc
niederlec <claude.niederlender@gmail.com>
2018-05-04 12:46:55 +0200

    Allow overloading of "backup_path" field with ELASTIC_REPO envrionment variable

--- es.js
+++ es.js
@@ -5,10 +5,11 @@
 // =================================================================
 
 var elasticUrl = process.env.ELASTIC_URL || "http://localhost:9200";
+var elasticRepo = process.env.ELASTIC_REPO || "/applis/conditor/home/backup";
 
 module.exports = {
 	host: elasticUrl,
 	index:"records",
 	type: "record",
-	backup_path:"/applis/conditor/home/backup"
+	backup_path:elasticRepo
 };
