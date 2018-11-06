10b5be4d7fa7203c87dbd8b0c0abeafe941356ba
niederlec <claude.niederlender@gmail.com>
2018-11-06 15:20:52 +0100

    make Elasticsearch index and type customizable by environment variables
    "records" and "record" are default values
    Environment variables are ELASTIC_INDEX and ELASTIC_TYPE

--- es.js
+++ es.js
@@ -6,10 +6,12 @@
 
 var elasticUrl = process.env.ELASTIC_URL || "http://localhost:9200";
 var elasticRepo = process.env.ELASTIC_REPO || "/applis/conditor/home/backup";
+var elasticIndex = process.env.ELASTIC_INDEX || "records";
+var elasticType = process.env.ELASTIC_TYPE || "record";
 
 module.exports = {
 	host: elasticUrl,
-	index:"records",
-	type: "record",
+	index: elasticIndex,
+	type: elasticType,
 	backup_path:elasticRepo
 };
