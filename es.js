ab3eca0b48c0f720197cc6ec77cdfbc45ac4c567
niederle <claude.niederlender@inist.fr>
2021-09-02 17:20:40 +0200

    fix/ES7-compatibility: remove Elasicsearch record type from config, and add auth param

--- es.js
+++ es.js
@@ -7,11 +7,11 @@
 var elasticUrl = process.env.ELASTIC_URL || "http://localhost:9200";
 var elasticRepo = process.env.ELASTIC_REPO || "/applis/conditor/home/backup";
 var elasticIndex = process.env.ELASTIC_INDEX || "records";
-var elasticType = process.env.ELASTIC_TYPE || "record";
+var elasticAuth = process.env.ELASTIC_AUTH || "elastic:elastic";
 
 module.exports = {
 	host: elasticUrl,
 	index: elasticIndex,
-	type: elasticType,
+	httpAuth: elasticAuth,
 	backup_path:elasticRepo
 };
