c5c858104510377d00f820f46e8048925ee91912
dreptin <clement.dreptin@inist.fr>
2022-02-02 10:43:47 +0100

    chore(styling): normalized the indent style

--- es.js
+++ es.js
@@ -1,17 +1,15 @@
-"use strict";
-
 // =================================================================
 //	Déclaration des paramètres de connexion
 // =================================================================
 
-var elasticUrl = process.env.ELASTIC_URL || "http://localhost:9200";
-var elasticRepo = process.env.ELASTIC_REPO || "/applis/conditor/home/backup";
-var elasticIndex = process.env.ELASTIC_INDEX || "records";
-var elasticAuth = process.env.ELASTIC_AUTH || "elastic:elastic";
+const elasticUrl = process.env.ELASTIC_URL || 'http://localhost:9200';
+const elasticRepo = process.env.ELASTIC_REPO || '/applis/conditor/home/backup';
+const elasticIndex = process.env.ELASTIC_INDEX || 'records';
+const elasticAuth = process.env.ELASTIC_AUTH || 'elastic:elastic';
 
 module.exports = {
 	host: elasticUrl,
 	index: elasticIndex,
 	httpAuth: elasticAuth,
-	backup_path:elasticRepo
+	backup_path: elasticRepo,
 };
